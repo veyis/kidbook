c03a3fba3aeb0c07b5ca0bbbfc2cbbd527a084d1
Can V Akgul <vakgul@gmail.com>
2024-09-19 12:43:22 -0700

    v3

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,3 +1,4 @@
+'use client';
 import Image from "next/image";
 import React, { useState } from "react";
 import { Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenuToggle, NavbarMenu, NavbarMenuItem, Link, Button } from "@nextui-org/react";
@@ -27,17 +28,26 @@ function Header() {
 
     ]
 
+    const [isMenuOpen, setIsMenuOpen] = useState(false);    
+
+
 
 
   return (
-    <Navbar maxWidth="full" >
+    <Navbar maxWidth="full" onMenuOpenChange={setIsMenuOpen} >
       <NavbarContent>
+        <NavbarMenuToggle
+        aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+        className="sm:hidden"
+        
+        />
+
         <NavbarBrand>
           <Image src={'/logo.svg'} alt="Logo" width={40} height={40} />
           <h2 className="font-bold text-2xl text-primary ml-3">Story Book</h2>
         </NavbarBrand>
       </NavbarContent>
-      <NavbarContent justify="center" >
+      <NavbarContent justify="center" className="hidden sm:flex" >
         {MenuList.map((item, index) => (
             <NavbarItem className="text-xl text-primary font-medium hover:underline mx-2" key={index}>
                 <Link href={item.path
@@ -53,8 +63,16 @@ function Header() {
                 <Link href="/register">Register</Link>
             </NavbarItem> */}
         </NavbarContent>
+        <NavbarMenu>
+            {MenuList.map((item, index) => (
+                <NavbarMenuItem key={index}>
+                    <Link href={item.path
+                    }>{item.name}</Link>
+                </NavbarMenuItem>
+            ))}
+            </NavbarMenu>
 
-
+    
 
     </Navbar>
   );
