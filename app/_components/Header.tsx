4e75f6001af4717b162a9c26915dd6b7e72c30c1
Can V Akgul <vakgul@gmail.com>
2024-09-19 11:55:51 -0700

    v3

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,62 +1,61 @@
-'use client'
 import Image from "next/image";
 import React, { useState } from "react";
-import { Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenuToggle, NavbarMenu, NavbarMenuItem, Link } from "@nextui-org/react";
-import myLogo from "../../public/logo.svg";
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenuToggle, NavbarMenu, NavbarMenuItem, Link, Button } from "@nextui-org/react";
+import path from "path";
+import { p } from "framer-motion/client";
+
 
 function Header() {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const menuItems = [
-    "Home",
-    "About",
-    "Services",
-    "Contact",
-  ];
+    const MenuList = [
+        {
+            name: "Home",
+            path: "/"
+        },
+        {
+            name: "About",
+            path: "/about"
+        },
+        {
+            name: "Contact",
+            path: "/contact"
+        },
+        {
+            name: "Blog",
+            path: "/blog"
+        },
+
+    ]
+
+
 
   return (
-    <Navbar onMenuOpenChange={setIsMenuOpen}>
+    <Navbar maxWidth="full" >
       <NavbarContent>
-        <NavbarMenuToggle
-          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
-          className="sm:hidden"
-        />
         <NavbarBrand>
-          <Image 
-            src={myLogo} 
-            alt="Logo" 
-            width={100} 
-            height={100} 
-            style={{ width: 'auto', height: 'auto' }}
-            priority
-          />
+          <Image src={'/logo.svg'} alt="Logo" width={40} height={40} />
+          <h2 className="font-bold text-2xl text-primary ml-3">Story Book</h2>
         </NavbarBrand>
       </NavbarContent>
-
-      <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        {menuItems.map((item, index) => (
-          <NavbarItem key={`${item}-${index}`}>
-            <Link color="foreground" href="#">
-              {item}
-            </Link>
-          </NavbarItem>
+      <NavbarContent justify="center" >
+        {MenuList.map((item, index) => (
+            <NavbarItem className="text-xl text-primary font-medium hover:underline mx-2" key={index}>
+                <Link href={item.path
+                }>{item.name}</Link>
+            </NavbarItem>
         ))}
-      </NavbarContent>
+        </NavbarContent>
+        <NavbarContent justify="end">
+            <NavbarItem >
+                <Button color="primary" >Get Started</Button>
+            </NavbarItem>
+            {/* <NavbarItem>
+                <Link href="/register">Register</Link>
+            </NavbarItem> */}
+        </NavbarContent>
 
-      <NavbarMenu>
-        {menuItems.map((item, index) => (
-          <NavbarMenuItem key={`${item}-${index}`}>
-            <Link
-              color="foreground"
-              className="w-full"
-              href="#"
-              size="lg"
-            >
-              {item}
-            </Link>
-          </NavbarMenuItem>
-        ))}
-      </NavbarMenu>
+
+
     </Navbar>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import Header from "./_components/Header";
 export default function Home() {
   return (
   
-    <div>
+    <div className="bg-[#292929]">
 
       <Header />
 
